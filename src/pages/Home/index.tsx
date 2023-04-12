efad922be8fbfb4fafde5d127bb2099b7b76b078
Bianca Sakaniwa <bianca.sakaniwa@dbserver.br>
2023-04-12 18:01:15 -0300

    refactor: melhorando nome dos componentes novos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,8 +2,8 @@ import { Box } from "@mui/material";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import PesquisarCidade from "../../components/PesquisarCidade";
-import { Hoje } from "../../components/Hoje";
-import { ProximosDias } from "../../components/ProximosDias";
+import { Hoje } from "../../components/TempoHoje";
+import { ProximosDias } from "../../components/TempoProximosDias";
 
 export default function Home() {
     return (
